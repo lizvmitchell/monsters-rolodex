f68d1e9964923f4135f1281243b3e6ceeff3faa8
Liz Mitchell <lizvmitchell@gmail.com>
2020-09-28 20:20:10 -0700

    Updates

---
+++ precache-manifest.06dc095dd0a94e93b32a10a45d82d55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55563c18cc032f67a53a6ff9601bba4f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "28123141c07dd0182f57",
+    "url": "/monsters-rolodex/static/css/main.f6e559bf.chunk.css"
+  },
+  {
+    "revision": "7e93ee2c2621fb53e8f7",
+    "url": "/monsters-rolodex/static/js/2.74e548cf.chunk.js"
+  },
+  {
+    "revision": "d802ab49fdc0c956b7f3c7461f4294c5",
+    "url": "/monsters-rolodex/static/js/2.74e548cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28123141c07dd0182f57",
+    "url": "/monsters-rolodex/static/js/main.d6c6e351.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
